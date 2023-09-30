5a4c6778968aa2b07d5ae52c22136ddaa65d20f1
uzairjacobs <93554483+uzairjacobs@users.noreply.github.com>
2023-09-30 03:51:45 +0200

    updated readme

---
+++ README.md
@@ -0,0 +1,34 @@
+<!-- omit in toc -->
+# Tally counter app
+
+Introducing our user-friendly Tally Count App, designed with simplicity in mind for all your counting needs. With just a tap of your finger, you can effortlessly increase your tally count using the convenient incrementing button. Whether you're tracking inventory, taking attendance, or keeping score, our app makes counting a breeze. But that's not all – we've also included a decrementing button for those moments when you need to correct a count quickly. No more fumbling through complex menus or confusing settings; our app is straightforward and intuitive, ensuring that anyone can use it with ease. Say goodbye to pen and paper and hello to the future of tally counting!
+
+
+<!-- omit in toc -->
+## Table of Contents
+- [Features](#features)
+- [Requirements](#requirements)
+- [Getting started](#getting-started)
+
+## Features
+- Counting with incrementing button
+- Counting with decrementing button
+- Simple and user-friendly
+
+
+ ## Requirements
+- An IDE like [VIsual Studio Code](https://code.visualstudio.com)
+- Basic [HTML, CSS and JavaScript](https://developer.mozilla.org/en-US/docs/Learn)
+- Browser like [Chrome](https://www.google.com/chrome)
+
+## Getting started
+
+1. Clone the repository
+
+```
+git clone
+```
+
+2. Run localhost server
+
+3. Open `index.html`
--- script.js
+++ script.js
@@ -1,43 +1,74 @@
-const MAX_NUMBER = 15;
-const MIN_NUMBER = -5;
-const STEP_AMOUNT = 5;
+
+//@ts-check
+
+/**
+  * Maximum allowed number.
+  * @constant {number}
+  */
+
+const MAX_NUMBER = 15
+
+/**
+ * Minimum allowed number.
+ * @constant {number}
+ */
+
+const MIN_NUMBER = -5
+/**
+ * The number input element.
+ * @type {HTMLInputElement}
+ */
+
+const number = document.querySelector('[data-key="number"]')
+/**
+ * The subtract button element.
+ * @type {HTMLButtonElement}
+ */
+
+const subtract = document.querySelector('[data-key="subtract"]')
+/**
+ * The add button element.
+ * @type {HTMLButtonElement}
+ */
 
 
-const number = document.querySelector('[data-key="number"]');
-const subtract = document.querySelector('[data-key="subtract"]');
-const add = document.querySelector('[data-key="add"]');
-
-const subtractHandler = () => {
-  const newValue = parseInt(number.value) - STEP_AMOUNT;
-  number.value = newValue;
-
-  if (add.disabled === true ) {
-    add.disabled = false
-  }
-
-  if (newValue <= MIN_NUMBER){
-    subtract.disabled = true;
-  } else {
-    subtract.disabled = false;
-  }
-}
-
-const addHandler = () => {
-  const newValue = parseInt(number.value) + STEP_AMOUNT;
-  number.value = newValue;
-
-  if (subtract.disabled === true ) {
-    subtract.disabled = false
-  }
+const add = document.querySelector('[data-key="add"]')
 
 
-  if (newValue >= MAX_NUMBER) {
-    add.disabled = true;
-  } else {
-    add.disabled = false;
-  }
+
+/**
+ * Event handler for the subtract button click event.
+ * @prop {Function} subtractHandler - Event handler for the subtract button click event.
+ * @param {Event} event - The click event object.
+ */
+const subtractHandler = (event) => {
+    /**
+     * The new value after subtracting one from the current number value.
+     * @type {number}
+     * @prop {number} newValue - The new value after subtracting one.
+     */
+    const newValue = parseInt(number.value) - 1;
+
+    number.value = newValue;
+
+    if (newValue <= MIN_NUMBER) {
+        subtract.disabled = true;
+    } else {
+        subtract.disabled = false;
+    }
 };
 
-subtract.addEventListener("click", subtractHandler);
 
-add.addEventListener("click", addHandler);
+const addHandler = () => {
+    const newValue = parseInt(number.value) + 1
+    number.value = newValue
+
+    if (newValue >= MAX_NUMBER) {
+        add.disabled = true
+    } else {
+        add.disabled = false
+    }
+}
+
+subtract.addEventListener('click', subtractHandler)
+add.addEventListener('click', addHandler)
